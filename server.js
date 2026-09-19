8eca044fb8c9bbbce54e419668d972f96cd09f71
Abdullah <adedejiabdullah26@gmail.com>
2026-09-19 16:01:16 +0100

    on setting the backend up

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-import mongoose from 'mongoose';
-import cors from 'cors';
-import express from 'express';
-import dotenv from 'dotenv';
-import multer from 'multer';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import multer from 'multer';
+
+const mongoose = require('mongoose');
+const cors = require('cors');
+const express = require('express');
+const dotenv = require('dotenv');
+const multer = require('multer');
+
 
 dotenv.config();
 
+const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
