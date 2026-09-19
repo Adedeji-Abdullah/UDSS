330a596f4f9671371506ad99dbb2dc522d0f69d0
Abdullah <adedejiabdullah26@gmail.com>
2026-09-19 15:55:46 +0100

    added package.json()

---
+++ server.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+import multer from 'multer';
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+
