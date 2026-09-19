2a227fc1fb33726128b552740be29f8b8d7045ec
Abdullah <adedejiabdullah26@gmail.com>
2026-09-19 16:13:09 +0100

    on setting the backend up

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/udss', (req, res) => {
+    console.log("UDSS!!!")
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 })
